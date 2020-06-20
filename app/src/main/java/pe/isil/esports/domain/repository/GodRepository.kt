8010ad0a955272d5118912ec5e4a496d8f66d4f3
unknown <xris0711@gmail.com>
2020-06-19 23:33:56 -0500

    Updated God View

--- app/src/main/java/pe/isil/esports/domain/repository/GodRepository.kt
+++ app/src/main/java/pe/isil/esports/domain/repository/GodRepository.kt
@@ -5,7 +5,11 @@ import pe.isil.esports.domain.model.God
 import pe.isil.esports.domain.vo.OperationResult
 
 interface GodRepository {
-    fun getAll(): Flow<OperationResult<List<God>>>
+    fun getGuardians(): Flow<OperationResult<List<God>>>
+    fun getWarriors(): Flow<OperationResult<List<God>>>
+    fun getHunters(): Flow<OperationResult<List<God>>>
+    fun getMages(): Flow<OperationResult<List<God>>>
+    fun getAssassins(): Flow<OperationResult<List<God>>>
     fun create(god: God): Flow<OperationResult<God>>
     fun update(id: Long, god: God): Flow<OperationResult<God>>
     fun delete(id: Long): Flow<OperationResult<String>>
--- app/src/main/java/pe/isil/esports/domain/usecase/GodUseCase.kt
+++ app/src/main/java/pe/isil/esports/domain/usecase/GodUseCase.kt
@@ -7,8 +7,24 @@ import pe.isil.esports.domain.vo.OperationResult
 
 class GodUseCase(private val repository: GodRepository) {
 
-    fun getAll(): Flow<OperationResult<List<God>>> {
-        return repository.getAll()
+    fun getGuardians(): Flow<OperationResult<List<God>>>{
+        return repository.getGuardians()
+    }
+
+    fun getWarriors(): Flow<OperationResult<List<God>>>{
+        return repository.getWarriors()
+    }
+
+    fun getHunters(): Flow<OperationResult<List<God>>>{
+        return repository.getHunters()
+    }
+
+    fun getMages(): Flow<OperationResult<List<God>>>{
+        return repository.getMages()
+    }
+
+    fun getAssassins(): Flow<OperationResult<List<God>>>{
+        return repository.getAssassins()
     }
 
     fun create(god: God): Flow<OperationResult<God>> {
